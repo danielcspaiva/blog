7b477f2c9e587eb28c2bdcf19dfd2c2098af144d
Daniel Paiva <daniel@quartoavista.com.br>
2025-03-10 23:57:58 -0300

    set domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://dcsp.vercel.app",
+  site: "https://dcsp.dev",
   integrations: [
     sitemap(), 
     mdx(),
